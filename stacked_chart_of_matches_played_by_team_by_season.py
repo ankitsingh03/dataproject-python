ab935f3c96d363edd2461c54fe7db7b4e8635951
ankit <singh.ankit240@gmail.com>
2020-11-06 22:30:34 +0530

    detailing

--- stacked_chart_of_matches_played_by_team_by_season.py
+++ stacked_chart_of_matches_played_by_team_by_season.py
@@ -5,45 +5,52 @@ from operator import add
 
 
 def data_collection():
+    """
+    collect all the data according to this form \
+    {season: {team: number of matches played in one season}} \
+    and list of teams
+    """
     season = defaultdict(list)
     teams = set()
     with open('matches.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         csv_reader.__next__()
+        # creating dict {season: list of teams name}
         for line in csv_reader:
             season[line[1]].extend([line[4], line[5]])
             teams.update({line[4], line[5]})
 
+    # creating dict {season: {team: number of matches played in one season}}
     total = {}
     for p, q in season.items():
         for i in q:
             total[p] = {i: q.count(i) for i in q}
 
-    return total, teams
+    return total, list(teams)
 
 
-def plot_graph(data, team):
+def plot_graph(data, teams):
     year = sorted(data.keys())
-    team = list(team)
 
+    # create dict according to {team: [number of matches per season]}
     team_data = defaultdict(list)
-    for i in team:
+    for i in teams:
         for j in year:
             team_data[i].append(data[j].get(i, 0))
 
     plot = []
-    plot.append(plt.bar(year, team_data[team[0]]))
-    sum = team_data[team[0]]
-    for i in range(1, len(team)):
+    plot.append(plt.bar(year, team_data[teams[0]]))
+    sum = team_data[teams[0]]
+    for i in range(1, len(teams)):
         if i > 1:
-            sum = list(map(add, sum, team_data[team[i-1]]))
-        plot.append(plt.bar(year, team_data[team[i]], bottom=sum))
+            sum = list(map(add, sum, team_data[teams[i-1]]))
+        plot.append(plt.bar(year, team_data[teams[i]], bottom=sum))
 
     plt.ylabel('Scores')
     plt.title('Matches Played By Team By Season')
     plt.xticks(year)
     plt.yticks(range(0, 210, 10))
-    plt.legend((i[0] for i in plot), (team))
+    plt.legend((i[0] for i in plot), (teams))
 
     plt.show()
 
